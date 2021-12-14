22ff6296dfb59ede0de5e46b570b846196586f8a
Josh <joshdanielrea@gmail.com>
2021-12-14 14:33:25 -0600

    ridereview

--- client/src/pages/RideReview.js
+++ client/src/pages/RideReview.js
@@ -15,9 +15,9 @@ useEffect (() => {
 
 const getRides = async () => {
     const response = await axios.get(
-      `http://localhost:3001/api/rides/${id}`
+      `http://localhost:3001/api/rides/${props.id}`
     )
-    setRides(response)
+    setRides( id )
   }
 
 // console.log(id)
