dcfdaa5af4c377e3ae94133c6e4908b79b2d1428
domdelorenzo <delorenzo.breed@gmail.com>
2021-12-13 18:27:46 -0600

    Added review type to Ride schema and title type to Review schema

--- models/review.js
+++ models/review.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const Review = new Schema(
   {
+    title: { type: String, required: true },
     name: { type: String, required: true },
     username: { type: String, required: true },
     description: { type: String, required: true },
